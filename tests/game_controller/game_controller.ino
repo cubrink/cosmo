06cb875fc27279814796d8bb5e3e14e803d4412a
Curtis Brinker <curtisbrnrk@gmail.com>
2021-11-06 06:25:13 -0500

    Added code for button based controller

---
+++ tests/game_controller/game_controller.ino
@@ -0,0 +1,129 @@
+#include <BleGamepad.h>    // https://github.com/lemmingDev/ESP32-BLE-Gamepad
+
+const int e_stop = 14;         // Red
+
+const int NUM_CONTROLLER_BUTTONS = 8;
+// Define button pins
+const int unknown_button = 12; // Black
+
+const int fast_travel = 17;    // Right white
+const int select_button = 4;   // Middle white
+const int toggle_mode = 16;    // Left white
+
+const int speed_up = 15;       // Green right
+const int speed_down = 2;      // Green left 
+const int time_speed_up = 26;  // Blue right
+const int time_speed_down = 27;// Blue left
+
+const int joy_left_unknown = 35;
+const int joy_left_forward_backward = 34;
+const int joy_right_roll = 39;
+const int joy_right_pitch = 36; 
+
+const int led_connection_status = 18;
+const int led_move_left = 5;
+const int led_move_right = 19;
+
+// Define button states
+uint8_t curr_state;
+uint8_t prev_state;
+uint8_t changed;
+
+bool emergency;
+
+BleGamepad gamepad;
+
+uint8_t read_buttons();
+void update_state();
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  gamepad.begin();
+
+  // Initialize buttons as inputs/outputs
+  pinMode(unknown_button, INPUT);
+  pinMode(e_stop, INPUT);
+  pinMode(fast_travel, INPUT);
+  pinMode(select_button, INPUT);
+  pinMode(toggle_mode, INPUT);
+  pinMode(speed_up, INPUT);
+  pinMode(speed_down, INPUT);
+  pinMode(time_speed_up, INPUT);
+  pinMode(time_speed_down, INPUT);
+  pinMode(joy_left_unknown, INPUT);
+  pinMode(joy_left_forward_backward, INPUT);
+  pinMode(joy_right_roll, INPUT);
+  pinMode(joy_right_pitch, INPUT);
+
+  pinMode(led_connection_status, OUTPUT);
+  pinMode(led_move_left, OUTPUT);
+  pinMode(led_move_right, OUTPUT);
+  digitalWrite(led_connection_status, LOW);
+  digitalWrite(led_move_left, LOW);
+  digitalWrite(led_move_right, LOW);
+
+  // Initialize button states
+  curr_state = 0;
+  prev_state = 0;
+  changed = 0;
+
+  // flag set by pressing the e_stop button
+  emergency = false;
+}
+
+void loop() {
+  emergency |= !digitalRead(e_stop); // Set emergency flag if the user presses the e_stop button
+                                     // E-stop is active low for safety.
+                                     // If the circuit becomes damaged, a low value is read
+                                     // which will flag an emergency
+  update_state();
+  if (!emergency && gamepad.isConnected()) {
+      // Skip while gamepad is not connected
+      for (int i = 0; i < NUM_CONTROLLER_BUTTONS; i++) {
+          uint8_t button_code = (i+1);
+          uint8_t mask = (1 << i);
+        
+          if (!(changed & mask)) {
+            // This button hasn't changed, skip
+            continue;
+          }
+          if (mask & curr_state) {
+            // Button has been pressed
+            gamepad.press(button_code);
+          }
+          else {
+            // Button has been released
+            gamepad.release(button_code);
+          }
+      }
+  }
+  else {
+    // Note: We also consider a gamepad disconnect as an emergency stop
+    Serial.println("User has pressed the e_stop, quitting");
+    // TODO: Continiously send stop motors message
+  }
+  delay(10);
+}
+
+uint8_t read_buttons() {
+  // Returns an 8 bit value where each bit value represents if a button is pressed
+  return (
+      (digitalRead(unknown_button)  << 0)
+    | (digitalRead(fast_travel)     << 1)
+    | (digitalRead(select_button)   << 2)
+    | (digitalRead(toggle_mode)     << 3)
+    | (digitalRead(speed_up)        << 4)
+    | (digitalRead(speed_down)      << 5)
+    | (digitalRead(time_speed_up)   << 6)
+    | (digitalRead(time_speed_down) << 7)
+  );
+}
+
+void update_state() {
+  // Returns an 8 bit value where each bit represents if the state of the button has changed.
+  prev_state = curr_state;
+  curr_state = read_buttons();
+  changed = prev_state ^ curr_state; // XOR to check if values have changed.
+};
