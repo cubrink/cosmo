aa8fae5ab5ddebd3096c7295d5235b840bd6b0de
Curtis Brinker <curtisbrnrk@gmail.com>
2021-11-05 21:07:40 -0500

    Added example of basic uploading to ESP32

---
+++ tests/esp32_upload_test/esp32_test.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("Setup done");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello world!");
+}
