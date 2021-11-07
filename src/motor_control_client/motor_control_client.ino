d98d4c52ed1768d2c7cb0cf88b380209e30200be
Jeffrey Phillips <jspf73@mst.edu>
2021-11-06 20:08:44 -0500

    connection check with main controller

--- src/motor_control_client/motor_control_client.ino
+++ src/motor_control_client/motor_control_client.ino
@@ -7,18 +7,27 @@
  * 
  */
 
-// MY MAC Address:        94:B9:7E:E9:A2:E8
+// MY MAC Address:  94:B9:7E:E9:A2:E8
 
 #include <esp_now.h>
 #include <WiFi.h>
 #include <Wire.h>
 
+// MAC Address of game controller:  94:B9:7E:E9:91:BC 
+uint8_t broadcastAddress[] = {0x94, 0xB9, 0x7E, 0xE9, 0x91, 0xBC};
+
 // Relay pin numbers for powering motors
 const int motor1 = 12;
 const int motor2 = 14;
 
+// Onboard LED indicator
+const int onboard_led = 2;
+
 // Received data
 uint8_t rcvd_data;
+
+// Connection Status (false = not connected, true = connected)
+bool connection_status = false;
 
 // Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -30,44 +39,56 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(rcvd_data);
 
   // if motor1 = 1 and motor2 = 0
-  if((rcvd_data & 0b00000001) && !(rcvd_data & 0b00000010))
+  if(connection_status && ((rcvd_data & 0b00000001) && !(rcvd_data & 0b00000010)))
   {
     // turn on motor1
-    Serial.println("Motor 1 ON");
     digitalWrite(motor1, HIGH);
+    //Serial.println("Motor 1 ON");
   }
   else
   {
     // turn off motor1
-    Serial.println("Motor 1 OFF");
     digitalWrite(motor1, LOW);
+    //Serial.println("Motor 1 OFF");
   }
   // if motor2 = 1 and motor1 = 0
-  if((rcvd_data & 0b00000010) && !(rcvd_data & 0b00000001))
+  if(connection_status && ((rcvd_data & 0b00000010) && !(rcvd_data & 0b00000001)))
   {
     // turn on motor2
-    Serial.println("Motor 2 ON");
     digitalWrite(motor2, HIGH);
+    //Serial.println("Motor 2 ON");
   }
   else
   {
     // turn off motor2
-    Serial.println("Motor 2 OFF");
     digitalWrite(motor2, LOW);
+    //Serial.println("Motor 2 OFF");
   }
   // if received data is not a value between 0 and 3
   if(rcvd_data >= 3 || rcvd_data < 0)
   {
     // turn off both motors
-    Serial.println("An Error has Occured!");
     digitalWrite(motor1, LOW);
     digitalWrite(motor2, LOW);
+    Serial.println("An Error has Occured!");
     
     // User must power cycle to reset motors
     while(true) 
     {
       delay(50);
     }
+  }
+}
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  if (status == 0){
+    connection_status = true;
+    Serial.println("Connected");
+  }
+  else{
+    connection_status = false;
+    Serial.println("Connection ERROR");
   }
 }
  
@@ -81,6 +102,10 @@ void setup() {
   digitalWrite(motor1, LOW);
   digitalWrite(motor2, LOW);
 
+  // LED initialization
+  pinMode(onboard_led, OUTPUT);
+  digitalWrite(onboard_led, LOW);
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -89,17 +114,41 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+
+  // Register for Send CB to get connection status to main controller
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register the main game controller (for connection status)
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // Add the controller
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
   
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
 }
  
 void loop() {
+  // Send message via ESP-NOW to verify connection with main controller
+  uint8_t to_send = 1;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &to_send, sizeof(to_send));
 
-  Serial.println("Waiting for Data");
-  delay(2000);
-
+  // Set LED status
+  if(connection_status)
+  {
+    digitalWrite(onboard_led, HIGH);
+  }
+  else
+  {
+    digitalWrite(onboard_led, LOW);
+  }
   
 
-  
+  // Request connection status every 500 ms
+  delay(500);
 }
